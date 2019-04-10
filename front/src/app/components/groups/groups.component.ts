7a9df8dc344cee99c6116e5a84ebbb2874f5a763
YassinElf <yassin.elfahim@gmail.com>
2019-04-10 23:00:00 +0200

    Work on front

--- front/src/app/components/groups/groups.component.ts
+++ front/src/app/components/groups/groups.component.ts
@@ -7,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GroupsComponent implements OnInit {
 
+  selectedItem: string;
+  items = [
+    {
+      name: 'name one'
+    },
+    {
+      name: 'name two'
+    }
+  ];
+  itemPluralMapping = {
+    'group': {
+      '=0': 'n\'avez aucun groupe',
+      '=1': 'un groupe',
+      'other': '# groupes'
+    }
+  };
   constructor() { }
 
   ngOnInit() {
   }
 
+
+  selectItem(item) {
+    this.selectedItem = item.name;
+  }
 }
--- front/src/app/components/supplier/supplier.component.ts
+++ front/src/app/components/supplier/supplier.component.ts
@@ -10,20 +10,152 @@ export class SupplierComponent implements OnInit {
   supplierNmb = 7;
   items = [
     {
-      status: false,
+      status: true,
+      name: 'vert',
+      number: '92343843284',
+      date: 1511950620,
+      dateInv: 1511950620,
+      urssaf: true,
+      kbis: true,
+      lnte: true
+    },
+    {
+      status: true,
+      name: 'Orange',
+      number: '92343843284',
+      date: 1511950620,
+      dateInv: 1511950620,
+      urssaf: true,
+      kbis: false,
+      lnte: false
+    },
+    {
+      status: true,
       name: 'Orange',
       number: '92343843284',
       date: 1511950620,
       dateInv: 1511950620,
       urssaf: true,
       kbis: true,
-      lnte: true,
-      
+      lnte: true
+    },
+    {
+      status: true,
+      name: 'Orange',
+      number: '92343843284',
+      date: 1511950620,
+      dateInv: 1511950620,
+      urssaf: true,
+      kbis: false,
+      lnte: false
+    },{
+      status: true,
+      name: 'Orange',
+      number: '92343843284',
+      date: 1511950620,
+      dateInv: 1511950620,
+      urssaf: true,
+      kbis: true,
+      lnte: true
+    },
+    {
+      status: true,
+      name: 'Orange',
+      number: '92343843284',
+      date: 1511950620,
+      dateInv: 1511950620,
+      urssaf: true,
+      kbis: false,
+      lnte: false
+    },{
+      status: true,
+      name: 'Orange',
+      number: '92343843284',
+      date: 1511950620,
+      dateInv: 1511950620,
+      urssaf: true,
+      kbis: true,
+      lnte: true
+    },
+    {
+      status: true,
+      name: 'Orange',
+      number: '92343843284',
+      date: 1511950620,
+      dateInv: 1511950620,
+      urssaf: true,
+      kbis: false,
+      lnte: false
+    },{
+      status: true,
+      name: 'Orange',
+      number: '92343843284',
+      date: 1511950620,
+      dateInv: 1511950620,
+      urssaf: true,
+      kbis: true,
+      lnte: true
+    },
+    {
+      status: true,
+      name: 'Orange',
+      number: '92343843284',
+      date: 1511950620,
+      dateInv: 1511950620,
+      urssaf: true,
+      kbis: false,
+      lnte: false
+    },{
+      status: true,
+      name: 'Orange',
+      number: '92343843284',
+      date: 1511950620,
+      dateInv: 1511950620,
+      urssaf: true,
+      kbis: true,
+      lnte: true
+    },
+    {
+      status: true,
+      name: 'Orange',
+      number: '92343843284',
+      date: 1511950620,
+      dateInv: 1511950620,
+      urssaf: true,
+      kbis: false,
+      lnte: false
+    },{
+      status: true,
+      name: 'Orange',
+      number: '92343843284',
+      date: 1511950620,
+      dateInv: 1511950620,
+      urssaf: true,
+      kbis: true,
+      lnte: true
+    },
+    {
+      status: true,
+      name: 'Orange',
+      number: '92343843284',
+      date: 1511950620,
+      dateInv: 1511950620,
+      urssaf: true,
+      kbis: false,
+      lnte: false
     }
-  ]
+  ];
   constructor() { }
 
   ngOnInit() {
   }
 
+
+  edit(item) {
+    console.log(item);
+  }
+
+  delete(item) {
+    console.log(item);
+  }
 }
