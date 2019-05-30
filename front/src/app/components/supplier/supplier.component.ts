d9526c26e9bfff63dbf9a5922c387bafffd55df1
YassinElf <yassin.elfahim@gmail.com>
2019-05-30 23:20:34 +0200

    w

--- front/src/app/components/supplier/supplier.component.ts
+++ front/src/app/components/supplier/supplier.component.ts
@@ -6,8 +6,10 @@ import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
 import { combineLatest, Subscription } from 'rxjs';
 import { SupplierTableComponent } from '../supplier-table/supplier-table.component';
 import { map, filter, catchError, mergeMap } from 'rxjs/operators';
-import fakeData from './fakeData.json';
 import { ProductService } from 'src/app/services/product.service';
+
+declare var require: any;
+const fakeData = require('./fakeData.json');
 
 @Component({
   selector: 'app-supplier',
@@ -22,10 +24,14 @@ export class SupplierComponent implements OnInit {
   modalRef: BsModalRef;
   supplierNmb = 7;
   groupSelect: String;
-  searchState: Boolean = true;
+  searchState: Boolean = false;
   selectedCompany: any = null;
   isLoading: Boolean = false;
   filteredList: Array<any>;
+  company: any = {"datasetid": "sirene_v3@public", "recordid": "baa8f969f38538021650304a70329a7fbfaa9585", "fields": {"siretsiegeunitelegale": "38793642000034", "soussectionetablissement": "Travaux de construction specialises", "trancheeffectifsunitelegale": "NN", "activiteprincipaleunitelegale": "43.34Z", "sexeunitelegale": "Masculin", "adresseetablissement": "GRAND CUL DE SAC", "groupeunitelegale": "Travaux de peinture et vitrerie", "classeetablissement": "Travaux de peinture et vitrerie", "prenomusuelunitelegale": "RAYMOND", "prenom1unitelegale": "RAYMOND", "activiteprincipaleetablissement": "43.34Z", "datederniertraitementetablissement": "2019-05-22T21:05:51+00:00", "siren": "387936420", "naturejuridiqueunitelegale": "Entrepreneur individuel", "nomenclatureactiviteprincipaleunitelegale": "NAFRev2", "nomunitelegale": "MEPHARA", "nomenclatureactiviteprincipaleetablissement": "NAFRev2", "divisionunitelegale": "Travaux de finition", "nombreperiodesetablissement": 1, "sectionunitelegale": "Construction", "nic": "00034", "siret": "38793642000034", "prenom2unitelegale": "CELESTIN", "nicsiegeunitelegale": "00034", "libellevoieetablissement": "GRAND CUL DE SAC", "etatadministratifetablissement": "Actif", "groupeetablissement": "Travaux de peinture et vitrerie", "datecreationunitelegale": "1992-04-27", "sectionetablissement": "Construction", "etatadministratifunitelegale": "Active", "categorieentreprise": "PME", "categoriejuridiqueunitelegale": "1000", "etablissementsiege": "oui", "codecommuneetablissement": "97701", "divisionetablissement": "Travaux de finition", "datecreationetablissement": "2019-05-16", "caractereemployeuretablissement": "Non", "soussectionunitelegale": "Travaux de construction specialises", "libellecommuneetablissement": "SAINT BARTHELEMY", "caractereemployeurunitelegale": "Non", "anneecategorieentreprise": "2016", "statutdiffusionetablissement": "O", "codepostaletablissement": "97133", "datederniertraitementunitelegale": "2019-05-22T21:05:51+00:00", "datedebutetablissement": "2019-05-16", "classeunitelegale": "Travaux de peinture et vitrerie", "statutdiffusionunitelegale": "O"}, "record_timestamp": "2019-05-23T00:04:00+00:00"}
+  goodCompany: Boolean;
+  modalState: String = 'enterSiret';
+
   itemPluralMapping = {
     'supplier': {
       '=0': 'n\'avez aucun fournisseur',
@@ -62,7 +68,7 @@ export class SupplierComponent implements OnInit {
 
     this.subscriptions.push(
       this.modalService.onHidden.subscribe(() => {
-        this.searchState = true;
+        this.searchState = false;
         this.companies.length = 0;
         this.searchCompanyID = '';
         this.unsubscribe();
@@ -81,13 +87,14 @@ export class SupplierComponent implements OnInit {
   }
 
   searchCompany(id: any) {
-    const params = '&q=' + id;
+    const params = '&rows=1&q=' + id;
     this.apiService.getCompany(params)
     .subscribe(res => {
       this.searchState = false;
       console.log('Res ', res);
-      if(res && res.records) {
-        this.companies = res.records;
+      if(res && res.records && res.records instanceof Array && res.records[0]) {
+        this.company = res.records[0];
+        console.log('this.company', this.company.fields);
       }
     }, err => {
       console.log('Error, ', err);
