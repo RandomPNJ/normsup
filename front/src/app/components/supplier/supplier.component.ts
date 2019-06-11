4c784461df415e81d9c4d42876716f87ecf4c00c
YassinElf <yassin.elfahim@gmail.com>
2019-06-11 08:51:45 +0200

    work

--- front/src/app/components/supplier/supplier.component.ts
+++ front/src/app/components/supplier/supplier.component.ts
@@ -7,6 +7,7 @@ import { combineLatest, Subscription } from 'rxjs';
 import { SupplierTableComponent } from '../supplier-table/supplier-table.component';
 import { map, filter, catchError, mergeMap } from 'rxjs/operators';
 import { ProductService } from 'src/app/services/product.service';
+import * as moment from 'moment';
 
 declare var require: any;
 const fakeData = require('./fakeData.json');
@@ -29,6 +30,7 @@ export class SupplierComponent implements OnInit {
   isLoading: Boolean = false;
   filteredList: Array<any>;
   company: any = {"datasetid": "sirene_v3@public", "recordid": "baa8f969f38538021650304a70329a7fbfaa9585", "fields": {"siretsiegeunitelegale": "38793642000034", "soussectionetablissement": "Travaux de construction specialises", "trancheeffectifsunitelegale": "NN", "activiteprincipaleunitelegale": "43.34Z", "sexeunitelegale": "Masculin", "adresseetablissement": "GRAND CUL DE SAC", "groupeunitelegale": "Travaux de peinture et vitrerie", "classeetablissement": "Travaux de peinture et vitrerie", "prenomusuelunitelegale": "RAYMOND", "prenom1unitelegale": "RAYMOND", "activiteprincipaleetablissement": "43.34Z", "datederniertraitementetablissement": "2019-05-22T21:05:51+00:00", "siren": "387936420", "naturejuridiqueunitelegale": "Entrepreneur individuel", "nomenclatureactiviteprincipaleunitelegale": "NAFRev2", "nomunitelegale": "MEPHARA", "nomenclatureactiviteprincipaleetablissement": "NAFRev2", "divisionunitelegale": "Travaux de finition", "nombreperiodesetablissement": 1, "sectionunitelegale": "Construction", "nic": "00034", "siret": "38793642000034", "prenom2unitelegale": "CELESTIN", "nicsiegeunitelegale": "00034", "libellevoieetablissement": "GRAND CUL DE SAC", "etatadministratifetablissement": "Actif", "groupeetablissement": "Travaux de peinture et vitrerie", "datecreationunitelegale": "1992-04-27", "sectionetablissement": "Construction", "etatadministratifunitelegale": "Active", "categorieentreprise": "PME", "categoriejuridiqueunitelegale": "1000", "etablissementsiege": "oui", "codecommuneetablissement": "97701", "divisionetablissement": "Travaux de finition", "datecreationetablissement": "2019-05-16", "caractereemployeuretablissement": "Non", "soussectionunitelegale": "Travaux de construction specialises", "libellecommuneetablissement": "SAINT BARTHELEMY", "caractereemployeurunitelegale": "Non", "anneecategorieentreprise": "2016", "statutdiffusionetablissement": "O", "codepostaletablissement": "97133", "datederniertraitementunitelegale": "2019-05-22T21:05:51+00:00", "datedebutetablissement": "2019-05-16", "classeunitelegale": "Travaux de peinture et vitrerie", "statutdiffusionunitelegale": "O"}, "record_timestamp": "2019-05-23T00:04:00+00:00"}
+  companyToAdd: any = {};
   goodCompany: Boolean;
   modalState: String = 'enterSiret';
 
@@ -56,6 +58,11 @@ export class SupplierComponent implements OnInit {
 
   ngOnInit() {
     this.itemsDisplay = [...this.items];
+    this.modalState = 'compInfo';
+    this.companyToAdd.denom = this.company.fields.denominationunitelegale;
+    this.companyToAdd.siren = this.company.fields.siren;
+    this.companyToAdd.adress = this.company.fields.adresseetablissement;
+    this.companyToAdd.dateCrea = moment(this.company.fields.datecreationetablissement, "YYYY/MM/DD").format('DD/MM/YYYY');
   }
 
   openModal(template: TemplateRef<any>) {
@@ -69,8 +76,9 @@ export class SupplierComponent implements OnInit {
     this.subscriptions.push(
       this.modalService.onHidden.subscribe(() => {
         this.searchState = false;
-        this.companies.length = 0;
+        // this.company = {};
         this.searchCompanyID = '';
+        // this.companyToAdd = {};
         this.unsubscribe();
       })
     );
@@ -94,11 +102,19 @@ export class SupplierComponent implements OnInit {
       console.log('Res ', res);
       if(res && res.records && res.records instanceof Array && res.records[0]) {
         this.company = res.records[0];
+        this.fillCompany(res.records[0].fields);
         console.log('this.company', this.company.fields);
       }
     }, err => {
       console.log('Error, ', err);
     });
+  }
+
+  fillCompany(record: any) {
+    this.companyToAdd.denom = record.denominationunitelegale;
+    this.companyToAdd.siren = record.siren;
+    this.companyToAdd.adress = record.adresseetablissement;
+    this.companyToAdd.dateCrea = moment(record.datecreationetablissement, 'YYYY/MM/DD').format('DD/MM/YYYY');
   }
 
   onCompanySelection(company: any) {
@@ -108,6 +124,6 @@ export class SupplierComponent implements OnInit {
   }
 
   addCompany(company: any) {
-    
+    console.log('Company', company);
   }
 }
