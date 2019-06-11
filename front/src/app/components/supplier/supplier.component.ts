a9bf09b7d9eea7a2301234c6de5496c22e23f17c
YassinElf <yassin.elfahim@gmail.com>
2019-06-12 00:20:24 +0200

    work

--- front/src/app/components/supplier/supplier.component.ts
+++ front/src/app/components/supplier/supplier.component.ts
@@ -29,10 +29,18 @@ export class SupplierComponent implements OnInit {
   selectedCompany: any = null;
   isLoading: Boolean = false;
   filteredList: Array<any>;
-  company: any = {"datasetid": "sirene_v3@public", "recordid": "baa8f969f38538021650304a70329a7fbfaa9585", "fields": {"siretsiegeunitelegale": "38793642000034", "soussectionetablissement": "Travaux de construction specialises", "trancheeffectifsunitelegale": "NN", "activiteprincipaleunitelegale": "43.34Z", "sexeunitelegale": "Masculin", "adresseetablissement": "GRAND CUL DE SAC", "groupeunitelegale": "Travaux de peinture et vitrerie", "classeetablissement": "Travaux de peinture et vitrerie", "prenomusuelunitelegale": "RAYMOND", "prenom1unitelegale": "RAYMOND", "activiteprincipaleetablissement": "43.34Z", "datederniertraitementetablissement": "2019-05-22T21:05:51+00:00", "siren": "387936420", "naturejuridiqueunitelegale": "Entrepreneur individuel", "nomenclatureactiviteprincipaleunitelegale": "NAFRev2", "nomunitelegale": "MEPHARA", "nomenclatureactiviteprincipaleetablissement": "NAFRev2", "divisionunitelegale": "Travaux de finition", "nombreperiodesetablissement": 1, "sectionunitelegale": "Construction", "nic": "00034", "siret": "38793642000034", "prenom2unitelegale": "CELESTIN", "nicsiegeunitelegale": "00034", "libellevoieetablissement": "GRAND CUL DE SAC", "etatadministratifetablissement": "Actif", "groupeetablissement": "Travaux de peinture et vitrerie", "datecreationunitelegale": "1992-04-27", "sectionetablissement": "Construction", "etatadministratifunitelegale": "Active", "categorieentreprise": "PME", "categoriejuridiqueunitelegale": "1000", "etablissementsiege": "oui", "codecommuneetablissement": "97701", "divisionetablissement": "Travaux de finition", "datecreationetablissement": "2019-05-16", "caractereemployeuretablissement": "Non", "soussectionunitelegale": "Travaux de construction specialises", "libellecommuneetablissement": "SAINT BARTHELEMY", "caractereemployeurunitelegale": "Non", "anneecategorieentreprise": "2016", "statutdiffusionetablissement": "O", "codepostaletablissement": "97133", "datederniertraitementunitelegale": "2019-05-22T21:05:51+00:00", "datedebutetablissement": "2019-05-16", "classeunitelegale": "Travaux de peinture et vitrerie", "statutdiffusionunitelegale": "O"}, "record_timestamp": "2019-05-23T00:04:00+00:00"}
+  // company: any = {"datasetid": "sirene_v3@public", "recordid": "baa8f969f38538021650304a70329a7fbfaa9585", "fields": {"siretsiegeunitelegale": "38793642000034", "soussectionetablissement": "Travaux de construction specialises", "trancheeffectifsunitelegale": "NN", "activiteprincipaleunitelegale": "43.34Z", "sexeunitelegale": "Masculin", "adresseetablissement": "GRAND CUL DE SAC", "groupeunitelegale": "Travaux de peinture et vitrerie", "classeetablissement": "Travaux de peinture et vitrerie", "prenomusuelunitelegale": "RAYMOND", "prenom1unitelegale": "RAYMOND", "activiteprincipaleetablissement": "43.34Z", "datederniertraitementetablissement": "2019-05-22T21:05:51+00:00", "siren": "387936420", "naturejuridiqueunitelegale": "Entrepreneur individuel", "nomenclatureactiviteprincipaleunitelegale": "NAFRev2", "nomunitelegale": "MEPHARA", "nomenclatureactiviteprincipaleetablissement": "NAFRev2", "divisionunitelegale": "Travaux de finition", "nombreperiodesetablissement": 1, "sectionunitelegale": "Construction", "nic": "00034", "siret": "38793642000034", "prenom2unitelegale": "CELESTIN", "nicsiegeunitelegale": "00034", "libellevoieetablissement": "GRAND CUL DE SAC", "etatadministratifetablissement": "Actif", "groupeetablissement": "Travaux de peinture et vitrerie", "datecreationunitelegale": "1992-04-27", "sectionetablissement": "Construction", "etatadministratifunitelegale": "Active", "categorieentreprise": "PME", "categoriejuridiqueunitelegale": "1000", "etablissementsiege": "oui", "codecommuneetablissement": "97701", "divisionetablissement": "Travaux de finition", "datecreationetablissement": "2019-05-16", "caractereemployeuretablissement": "Non", "soussectionunitelegale": "Travaux de construction specialises", "libellecommuneetablissement": "SAINT BARTHELEMY", "caractereemployeurunitelegale": "Non", "anneecategorieentreprise": "2016", "statutdiffusionetablissement": "O", "codepostaletablissement": "97133", "datederniertraitementunitelegale": "2019-05-22T21:05:51+00:00", "datedebutetablissement": "2019-05-16", "classeunitelegale": "Travaux de peinture et vitrerie", "statutdiffusionunitelegale": "O"}, "record_timestamp": "2019-05-23T00:04:00+00:00"}
+  company: any;
   companyToAdd: any = {};
   goodCompany: Boolean;
   modalState: String = 'enterSiret';
+  addInterloc: Boolean = false;
+  interloc: any = {
+    name: "",
+    lastname: "",
+    phone: "",
+    mail: ""
+  };
 
   itemPluralMapping = {
     'supplier': {
@@ -58,27 +66,29 @@ export class SupplierComponent implements OnInit {
 
   ngOnInit() {
     this.itemsDisplay = [...this.items];
-    this.modalState = 'compInfo';
-    this.companyToAdd.denom = this.company.fields.denominationunitelegale;
-    this.companyToAdd.siren = this.company.fields.siren;
-    this.companyToAdd.adress = this.company.fields.adresseetablissement;
-    this.companyToAdd.dateCrea = moment(this.company.fields.datecreationetablissement, "YYYY/MM/DD").format('DD/MM/YYYY');
+    // this.modalState = 'compInfo';
+    // this.companyToAdd.denom = this.company.fields.denominationunitelegale;
+    // this.companyToAdd.siren = this.company.fields.siren;
+    // this.companyToAdd.adress = this.company.fields.adresseetablissement;
+    // this.companyToAdd.dateCrea = moment(this.company.fields.datecreationetablissement, "YYYY/MM/DD").format('DD/MM/YYYY');
   }
 
   openModal(template: TemplateRef<any>) {
     const _combine = combineLatest(
-      // this.modalService.onShow,
-      // this.modalService.onShown,
-      // this.modalService.onHide,
+      this.modalService.onShow,
+      this.modalService.onShown,
+      this.modalService.onHide,
       this.modalService.onHidden
     ).subscribe(() => this.changeDetection.markForCheck());
 
     this.subscriptions.push(
       this.modalService.onHidden.subscribe(() => {
-        this.searchState = false;
-        // this.company = {};
+        this.modalState = 'enterSiret';
+        this.company = {};
         this.searchCompanyID = '';
-        // this.companyToAdd = {};
+        this.companyToAdd = {};
+        this.addInterloc = false;
+        this.interloc = {name: "", lastname: "", phone: "", mail: ""};
         this.unsubscribe();
       })
     );
@@ -94,16 +104,26 @@ export class SupplierComponent implements OnInit {
     this.subscriptions = [];
   }
 
+  hideModal() {
+
+    if (!this.modalRef) {
+      return;
+    }
+  
+    this.modalService.hide(1);
+    this.modalRef = null;
+  }
+
   searchCompany(id: any) {
     const params = '&rows=1&q=' + id;
     this.apiService.getCompany(params)
     .subscribe(res => {
-      this.searchState = false;
-      console.log('Res ', res);
+      this.modalState = 'compInfo';
+      // console.log('Res ', res);
       if(res && res.records && res.records instanceof Array && res.records[0]) {
         this.company = res.records[0];
         this.fillCompany(res.records[0].fields);
-        console.log('this.company', this.company.fields);
+        // console.log('this.company', this.company.fields);
       }
     }, err => {
       console.log('Error, ', err);
