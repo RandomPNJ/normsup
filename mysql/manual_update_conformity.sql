d4a17e3245f7f2c98fc3c13c1838d0b642364e9e
YassinElf <yassin.elfahim@gmail.com>
2020-06-05 19:07:37 +0200

    Fix

--- mysql/manual_update_conformity.sql
+++ mysql/manual_update_conformity.sql
@@ -7,7 +7,7 @@
 
 ******************************/
 
-CREATE TEMPORARY TABLE tmp_conform_tbl SELECT * FROM supplier_conformity WHERE `start_date` = '2020-04-01 00:00:00';
-UPDATE tmp_conform_tbl SET `start_date` = '2020-05-01 00:00:00';
+CREATE TEMPORARY TABLE tmp_conform_tbl SELECT * FROM supplier_conformity WHERE `start_date` = '2020-05-01 00:00:00';
+UPDATE tmp_conform_tbl SET `start_date` = '2020-06-01 00:00:00';
 INSERT INTO supplier_conformity SELECT * FROM tmp_conform_tbl;
 DROP TEMPORARY TABLE IF EXISTS tmp_conform_tbl;
